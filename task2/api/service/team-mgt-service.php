b17cfa7e25e92114332f2535b8c663a7a22cb343
adebayo <adebayookutubo@gmail.com>
2024-05-07 20:32:14 +0100

    task2 initial backend setup: adding the necessary dependencies, helper modules and main api starter codes.

--- task2/api/service/team-mgt-service.php
+++ task2/api/service/team-mgt-service.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * This module handles team management functions such as:
+ * <li>creating new team(s)</li>
+ * <li>reading team information</li>
+ * <li>deleting team(s)</li>
+ * @author Adebayo .A. Okutubo
+ */
 
 
 ?>
